ff3b1f08a04f16d398b9251c9310392e06b32601
João Guiomar <joaofguiomar@gmail.com>
2018-05-09 08:37:27 +0100

    Requested changes

--- src/validators/isValidLicensePlate.js
+++ src/validators/isValidLicensePlate.js
@@ -5,11 +5,6 @@
 
 export default function isValidLicensePlate(plateNumber) {
   if (typeof plateNumber !== 'string') return false
-  if (
-    !/(?!(([a-zA-Z]{2})-([a-zA-Z]{2})-(\d{2}))|(\d{2})-([a-zA-Z]{2})-([a-zA-Z]{2})|(\d{2})-(\d{2})-(\d{2})|([a-zA-Z]{2})-([a-zA-Z]{2})-([a-zA-Z]{2}))((\d{2})|([a-zA-Z]{2}))-((\d{2})|(\w{2}))-((\d{2})|(\w{2}))/g.test(
-      plateNumber
-    )
-  )
-    return false
-  return true
+  const pattern = /(?!(([a-zA-Z]{2})-([a-zA-Z]{2})-(\d{2}))|(\d{2})-([a-zA-Z]{2})-([a-zA-Z]{2})|(\d{2})-(\d{2})-(\d{2})|([a-zA-Z]{2})-([a-zA-Z]{2})-([a-zA-Z]{2}))((\d{2})|([a-zA-Z]{2}))-((\d{2})|(\w{2}))-((\d{2})|(\w{2}))/g
+  return pattern.test(plateNumber)
 }
