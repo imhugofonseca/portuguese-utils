255ad6170544ed1c06d3a6e251b06d0b8da9ebf7
João Guiomar <joaofguiomar@gmail.com>
2018-05-10 10:03:08 +0100

    Added isValidCPE

---
+++ src/validators/isValidCPE.js
@@ -0,0 +1,40 @@
+import { FindValueOperator } from 'rxjs/operators/find'
+import { Z_ASCII } from 'zlib'
+import { QueueScheduler } from 'rxjs/scheduler/QueueScheduler'
+
+/**
+ * Validates if CPE (Código Ponto de Entrega) is in the valid format
+ * @param {String} cpe
+ */
+
+export default function isValidCPE(cpe) {
+  const ABValues = 'TRWAGMYFPDXBNJZSQVHLCKE'
+
+  // Must be a string
+  if (typeof cpe !== 'string') return false
+
+  // Trim and Uppercase
+  const parsed = cpe.replace(/\s+/g, '').toUpperCase()
+
+  // Must be exactly 20 characters long
+  if (parsed.length != 20) return false
+
+  // Validate with a pattern
+  // Rules: PT(4digits)(12digits)(2Characters)
+  const pattern = /PT(\d{4})(\d{12})([A-Z]{2})/g
+  if (!pattern.test(parsed)) return false
+
+  // Calculate verification characters
+  // Documentation:
+  // http://www.erse.pt/pt/legislacao/Legislacao/Attachments/770/res2E5978000F3B442890EAA1412A11DE3E.pdf
+  const digits = parseInt(parsed.replace(/[A-Z]/g, ''))
+  const remainder = digits % 529
+  const A = ABValues[Math.floor(remainder / 23)]
+  const B = ABValues[Math.floor(remainder % 23)]
+
+  // A and B must be in the CPE
+  if (parsed.indexOf(A) === -1 || parsed.indexOf(B) === -1) return false
+
+  // Should return true if all goes well
+  return true
+}
