13078fd5a542fc0309e22aa11f463bdab6fdfcca
Telmo Alves <tm_alves@live.com.pt>
2018-05-21 17:29:59 +0100

    Added holiday validator (

---
+++ src/helpers/holidays.js
@@ -0,0 +1,12 @@
+export default [
+  { day: 1, month: 0, holiday: 'Dia de Ano Novo' },
+  { day: 25, month: 3, holiday: 'Dia da Liberdade' },
+  { day: 1, month: 4, holiday: 'Dia do Trabalhador' },
+  { day: 10, month: 5, holiday: 'Dia de Portugal' },
+  { day: 15, month: 7, holiday: 'Assunção de Nossa Senhora' },
+  { day: 5, month: 9, holiday: 'Implantação da República' },
+  { day: 1, month: 10, holiday: 'Dia de Todos os Santos' },
+  { day: 1, month: 11, holiday: 'Restauração da Independência' },
+  { day: 8, month: 11, holiday: 'Dia da Imaculada Conceição' },
+  { day: 25, month: 11, holiday: 'Natal' }
+]
