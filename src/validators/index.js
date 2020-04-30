3185c74aa2cc213497ca9c272c05cf4fb09434fb
Gabriel Martins <gabtec@sapo.pt>
2020-04-30 18:36:13 +0100

    Add feature isValidNISS (#37)
    * Add feature isValidNISS
    
    Co-authored-by: Hugo <git@hugo.gg>

--- src/validators/index.js
+++ src/validators/index.js
@@ -7,6 +7,8 @@ import isValidCPECUI from './isValidCPECUI'
 import isValidCurrency from './isValidCurrency'
 import isValidHoliday from './isValidHoliday'
 import isValidNNU from './isValidNNU'
+import isValidNISS from './isValidNISS'
+
 
 export {
   isValidPostalCode,
@@ -17,5 +19,6 @@ export {
   isValidCPECUI,
   isValidCurrency,
   isValidHoliday,
-  isValidNNU
+  isValidNNU,
+  isValidNISS
 }
